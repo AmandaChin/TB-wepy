f86f278857f35416856dd5f9f5e91c3483ca4b60
AmandaChin <15732152800@163.com>
2018-11-18 18:48:12 +0800

    查询课程及勋章币宝石币

--- src/config.js
+++ src/config.js
@@ -3,16 +3,14 @@
 ======================================================== */
 
 // 域名
-var host = 'http://localhost:3000/api';
+//var host = 'http://localhost:3000/api';
 //var host = 'http://www.opengle.com:3000/api';
-var host2 = 'http://www.opengle.com:3000/api';
+var host = 'https://www.opengle.com:3000/api';
 
 // 下面的地址配合云端 Demo 工作
 export const service = {
   // 主域
-  host,
-
-  host2
+  host
 }
 
 export default {
